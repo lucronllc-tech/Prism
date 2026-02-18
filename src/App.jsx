1171a860e2c0b5cc4289cdde87926224c24d1333
lucronllc-tech <lucronllc@gmail.com>
2026-02-18 14:41:38 -0600

    Create App.jsx file with PRISM CORE v4.0 React component code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrismCore = () => {
+    return (
+        <div>
+            <h1>Welcome to PRISM CORE v4.0</h1>
+            <p>This is a React component for the PRISM CORE framework.</p>
+        </div>
+    );
+};
+
+export default PrismCore;
